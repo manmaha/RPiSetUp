51e41b0863cb653f82ab8c1162a085496c042d38
manmaha <manish.p.mahajan@gmail.com>
2019-07-31 23:54:48 +0800

    Update talkMac.sh

--- talkMac.sh
+++ talkMac.sh
@@ -1,12 +1,15 @@
 sudo apt-get install netatalk
 sudo apt-get install avahi-daemon
 sudo update-rc.d avahi-daemon defaults
-scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/afpd /etc/avahi/services/afpd.service
+echo " You will be asked for Study Macs Pasword"
+sudo scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/afpd /etc/avahi/services/afpd.service
 sudo /etc/init.d/avahi-daemon restart
 sudo apt-get install tightvncserver
+echo " Choose VNC Password"
 vncserver
 cd /etc/init.d
-scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/tightvncserver .
+echo " You will be asked for Study Macs Pasword"
+sudo scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/tightvncserver .
 
 sudo chmod +x tightvncserver
 sudo pkill Xtightvnc
@@ -14,7 +17,7 @@ sudo pkill Xtightvnc
 sudo /etc/init.d/tightvncserver start
 cd /etc/init.d
 sudo update-rc.d tightvncserver defaults
-
-scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/rfb /etc/avahi/services/rfb.service
+echo " You will be asked for Study Macs Pasword"
+sudo scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/rfb /etc/avahi/services/rfb.service
 
 sudo /etc/init.d/avahi-daemon restart
