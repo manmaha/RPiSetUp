c0c6792c34ff55973376674db461d4c4bc45e2e9
manmaha <manish.p.mahajan@gmail.com>
2019-07-31 23:33:59 +0800

    Add files via upload

---
+++ talkMac.sh
@@ -0,0 +1,20 @@
+sudo apt-get install netatalk
+sudo apt-get install avahi-daemon
+sudo update-rc.d avahi-daemon defaults
+scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/afpd afpd.service
+sudo /etc/init.d/avahi-daemon restart
+sudo apt-get install tightvncserver
+vncserver
+cd /etc/init.d
+scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/tightvncserver .
+
+sudo chmod +x tightvncserver
+sudo pkill Xtightvnc
+
+sudo /etc/init.d/tightvncserver start
+cd /etc/init.d
+sudo update-rc.d tightvncserver defaults
+
+scp Manish@StudyMac.local:/Users/Owner/Documents/OneDrive/RaspberryPi/SetUp/rfb /etc/avahi/services/rfb.service
+
+sudo /etc/init.d/avahi-daemon restart
